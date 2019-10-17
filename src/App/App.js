0e894e6e627d8211df792ba6fba3a2b3eec57e03
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-17 09:40:30 -0600

    Add Form component to App

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Form from '../components/Form/Form';
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-
+          <Form />
         </div>
         <div className='resy-container'>
           
